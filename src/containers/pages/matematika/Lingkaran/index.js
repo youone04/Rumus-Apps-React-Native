e71a6bbf8a20c1e8918c31d1b893709389afbcb3
youone04 <yudi.gunaone87@gmail.com>
2020-12-15 23:02:48 +0700

    memperbarui halaman input lingkaran

--- src/containers/pages/matematika/Lingkaran/index.js
+++ src/containers/pages/matematika/Lingkaran/index.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {View,Text,Image} from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
-
+import styles from './css';
 class Lingkaran extends Component {
     constructor(props){
         super(props);
@@ -28,21 +28,21 @@ class Lingkaran extends Component {
     render(){
         return(
             <ScrollView>
-                <Text style={{textAlign: 'center',fontSize: 20}}>Rumus Lingkaran</Text>
-                <View style={{flexDirection:'row',alignSelf:'center',marginRight:60,marginTop: 20}}>
-                <View style={{width: 160,height: 160,flexDirection:'row',alignSelf:'center'}}>
-                    <Image style={{width: '100%',height: '100%'}} source={require('../../../../assets/img/imgRumus/lingkaran.png')} />
+                <Text style={styles.header}>Rumus Lingkaran</Text>
+                <View style={styles.container}>
+                <View style={styles.coverimg}>
+                    <Image source={require('../../../../assets/img/imgRumus/lingkaran.png')} />
                 </View>
-                <Text style={{marginLeft: -60,marginTop: 50,fontSize: 20}}>{this.state.jari}</Text>
+                <Text style={styles.jari}>{this.state.jari}</Text>
                 </View>
-                    <View style={{flexDirection:'row',backgroundColor: 'white',width: '80%',marginBottom: 5,borderRadius: 4,shadowColor: 'black',alignSelf:'center',height:90,marginTop: 50,shadowColor: 'black',alignSelf:'center',height:90,marginTop: 50,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}>
-                        <Text style={{fontSize: 20,marginLeft: 20,marginTop: 10}}>{`Luas : ${this.state.hasil}`}</Text>
+                    <View style={styles.kotakHasil}>
+                        <Text style={styles.textHasil}>{`Luas : ${this.state.hasil}`}</Text>
                     </View>
-                    <View style={{alignSelf:'center',marginTop: 20}}>
-                    <TextInput onChangeText={this.menanganiInput}  placeholder="masukan jari-jari" style={{borderWidth:1,borderColor:'#E8E8E8',borderRadius: 25,height:50,fontSize: 14,paddingLeft: 45,paddingRight: 25,backgroundColor: 'white',width: 330,marginRight: 10,shadowColor: 'black',alignSelf:'center',shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}/>
+                    <View style={styles.coverTextInput}>
+                    <TextInput onChangeText={this.menanganiInput}  placeholder="masukan jari-jari" style={styles.textInput}/>
                     </View>
-                    <View style={{height: 40, width: '78%',backgroundColor: '#00acc1',borderRadius: 10,alignSelf:'center',marginTop: 15}}>
-                    <Text onPress={() => this.hitungLuas()} style={{fontSize:13,fontWeight:'bold',color:'white',textAlign:'center',marginTop: 10}}>HITUNG LUAS</Text>    
+                    <View style={styles.coverButton}>
+                    <Text onPress={() => this.hitungLuas()} style={styles.textButton}>HITUNG LUAS</Text>    
                     </View>
             </ScrollView>
         )
