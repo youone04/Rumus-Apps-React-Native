cdd9eeac1486fd2517faeb7b1b50e8b7b62d68f2
youone04 <yudi.gunaone87@gmail.com>
2020-12-16 19:48:36 +0700

    memperbarui tampilan input trapesium

---
+++ src/containers/pages/matematika/Trapesium/css.js
@@ -0,0 +1,102 @@
+import {StyleSheet} from 'react-native';
+import {
+    responsiveHeight,
+    responsiveWidth,
+    responsiveFontSize
+  } from "react-native-responsive-dimensions";
+const styles = StyleSheet.create({
+    header: {
+        textAlign: 'center',
+        fontSize: responsiveFontSize(3),
+        marginTop: responsiveHeight(2)
+    },
+    container: {
+        flexDirection:'row',
+        alignSelf:'center',
+        marginLeft: responsiveWidth(11),
+        marginTop: responsiveHeight(5)
+    },
+    sisiAtas: {
+        marginTop: responsiveHeight(-3.5),
+        fontSize: responsiveFontSize(2),
+        position:'absolute',
+        marginLeft: responsiveWidth(18)
+    },
+    sisiBawah:{
+        marginTop: responsiveHeight(15),
+        fontSize: responsiveFontSize(2),
+        position:'absolute',
+        marginLeft: responsiveWidth(18)
+    },
+    tinggi:{
+        position:'absolute',
+        marginLeft: responsiveWidth(12), 
+        marginTop: responsiveHeight(3),
+        fontSize: responsiveFontSize(2)
+    },
+    coverimg: {
+        width: responsiveWidth(65),
+        height: responsiveHeight(15),
+        flexDirection:'row',
+        alignSelf:'center'
+    },
+    kotakHasil: {
+        flexDirection:'row',
+        backgroundColor: 'white',
+        width: responsiveWidth(81),
+        marginBottom: responsiveHeight(1),
+        borderRadius: 4,
+        shadowColor: 'black',
+        alignSelf:'center',
+        height:responsiveHeight(15),
+        marginTop: responsiveHeight(8),
+        shadowColor: "#000",
+        shadowOffset: {width: 0,height: 4,},
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9
+    },
+    textHasil: {
+        fontSize: responsiveFontSize(2.6),
+        marginLeft: responsiveWidth(5),
+        marginTop: responsiveHeight(2)
+    },
+    coverTextInput: {
+        alignSelf:'center',
+        marginTop: responsiveHeight(1)
+    },
+    textInput: {
+        borderWidth:responsiveWidth(0.5),
+        borderColor:'#E8E8E8',
+        borderRadius: responsiveWidth(6),
+        height: responsiveHeight(7.7),
+        fontSize: responsiveFontSize(1.8),
+        paddingLeft: responsiveWidth(5),
+        paddingRight: responsiveWidth(2),
+        backgroundColor: 'white',
+        width: responsiveWidth(80),
+        marginRight: responsiveWidth(0),
+        shadowColor: "#000",
+        shadowOffset: {width: 0,height: 4,},
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+        marginTop: responsiveHeight(1.5)
+    },
+    coverButton:{
+        width: responsiveWidth(80),
+        backgroundColor: '#00acc1',
+        height: responsiveHeight(6),
+        borderRadius: responsiveWidth(3),
+        alignSelf:'center',
+        marginTop: responsiveHeight(3)
+    },
+    textButton: {
+        fontSize:responsiveFontSize(1.6),
+        fontWeight:'bold',
+        color:'white',
+        textAlign:'center',
+        marginTop: responsiveHeight(2)
+    }
+})
+export default styles;
--- src/containers/pages/matematika/Trapesium/index.js
+++ src/containers/pages/matematika/Trapesium/index.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {View,Text,Image} from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
-
+import styles from './css';
 class Trapesium extends Component {
     constructor(props){
         super(props);
@@ -15,7 +15,6 @@ class Trapesium extends Component {
     }
    
     inputAtas = (inputAtas) =>{
-        // console.log(inputsatu.target.value)
             this.setState({
                 sisiAtas: inputAtas
         })
@@ -32,7 +31,6 @@ class Trapesium extends Component {
         })
     }
     hitungLuas = () => {
-        // alert(this.state.sisi)
         let sisiAtas = parseInt(this.state.sisiAtas)
         let tinggi = parseInt(this.state.tinggi)
         let sisiBawah = parseInt(this.state.sisiBawah)
@@ -44,25 +42,25 @@ class Trapesium extends Component {
     render(){
         return(
             <ScrollView>
-                <Text style={{textAlign: 'center',fontSize: 20}}>Rumus Trapesium</Text>
-                <View style={{flexDirection:'row',alignSelf:'center',marginLeft: 20}}>
-                <View style={{width: 200,height: 100,flexDirection:'row',alignSelf:'center',marginTop: 50}}>
-                    <Image style={{width: '99%',height: '100%'}} source={require('../../../../assets/img/imgRumus/trapesium.png')} />
+                <Text style={styles.header}>Rumus Trapesium</Text>
+                <View style={styles.container}>
+                <View style={styles.coverimg}>
+                    <Image  source={require('../../../../assets/img/imgRumus/trapesium.png')} />
                 </View>
-                <Text style={{textAlign: 'center',fontSize: 20,marginLeft: 80,position:'absolute',marginTop: 20}}>{this.state.sisiAtas}</Text>
-                <Text style={{margin: 0,marginTop: 75,fontSize: 20,position:'absolute',marginLeft: 55}}>{this.state.tinggi}</Text>
-                <Text style={{textAlign: 'center',fontSize: 20,marginLeft: 80,position:'absolute',marginTop:150}}>{this.state.sisiBawah}</Text>
+                <Text style={styles.sisiAtas}>{this.state.sisiAtas}</Text>
+                <Text style={styles.tinggi}>{this.state.tinggi}</Text>
+                <Text style={styles.sisiBawah}>{this.state.sisiBawah}</Text>
                 </View>
-                    <View style={{flexDirection:'row',backgroundColor: 'white',width: '80%',marginBottom: 5,borderRadius: 4,shadowColor: 'black',alignSelf:'center',height:90,marginTop: 40,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}>
-                        <Text style={{fontSize: 20,marginLeft: 20,marginTop: 10}}>{`Luas : ${this.state.hasil} `}</Text>
+                    <View style={styles.kotakHasil}>
+                        <Text style={styles.textHasil}>{`Luas : ${this.state.hasil} `}</Text>
                     </View>
-                    <View style={{alignSelf:'center',marginTop: 20}}>
-                    <TextInput onChangeText={this.inputAtas}  placeholder="masukan sisi atas" style={{borderWidth:1,borderColor:'#E8E8E8',borderRadius: 25,height:50,fontSize: 14,paddingLeft: 45,paddingRight: 25,backgroundColor: 'white',width: 330,marginRight: 10,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}/>
-                    <TextInput onChangeText={this.inputTinggi}  placeholder="Masukan tinggi" style={{borderWidth:1,borderColor:'#E8E8E8',borderRadius: 25,height:50,fontSize: 14,paddingLeft: 45,paddingRight: 25,backgroundColor: 'white',width: 330,marginRight: 10,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9,marginTop: 10}}/>
-                    <TextInput onChangeText={this.inputBawah}  placeholder="Masukan sisi bawah" style={{borderWidth:1,borderColor:'#E8E8E8',borderRadius: 25,height:50,fontSize: 14,paddingLeft: 45,paddingRight: 25,backgroundColor: 'white',width: 330,marginRight: 10,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9,marginTop: 10}}/>
+                    <View style={styles.coverTextInput}>
+                    <TextInput onChangeText={this.inputAtas}  placeholder="masukan sisi atas" style={styles.textInput}/>
+                    <TextInput onChangeText={this.inputTinggi}  placeholder="Masukan tinggi" style={styles.textInput}/>
+                    <TextInput onChangeText={this.inputBawah}  placeholder="Masukan sisi bawah" style={styles.textInput}/>
                     </View>
-                    <View style={{width: '78%',backgroundColor: '#00acc1',height: 'auto',borderRadius: 10,alignSelf:'center',marginTop: 10,height: 40}}>
-                    <Text onPress={() => this.hitungLuas()} style={{fontSize:13,fontWeight:'bold',color:'white',textAlign:'center',marginTop: 6,marginTop: 13}}>HITUNG LUAS</Text>    
+                    <View style={styles.coverButton}>
+                    <Text onPress={() => this.hitungLuas()} style={styles.textButton}>HITUNG LUAS</Text>    
                     </View>
             </ScrollView>
         )
