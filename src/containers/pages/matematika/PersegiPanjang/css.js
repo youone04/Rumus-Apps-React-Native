e06b0f6923ee2918a40eac18825984ad1ff04e7b
youone04 <yudi.gunaone87@gmail.com>
2020-12-16 21:58:51 +0700

    memperbarui halaman input persegi anjang

---
+++ src/containers/pages/matematika/PersegiPanjang/css.js
@@ -0,0 +1,96 @@
+import {StyleSheet} from 'react-native';
+import {
+    responsiveHeight,
+    responsiveWidth,
+    responsiveFontSize
+  } from "react-native-responsive-dimensions";
+const styles = StyleSheet.create({
+    header: {
+        textAlign: 'center',
+        fontSize: responsiveFontSize(3),
+        marginTop: responsiveHeight(2)
+    },
+    container: {
+        flexDirection:'row',
+        alignSelf:'center',
+        marginLeft: responsiveWidth(11),
+        marginTop: responsiveHeight(5)
+    },
+    lebar: {
+        marginTop: responsiveHeight(8),
+        fontSize: responsiveFontSize(2),
+        position:'absolute',
+        marginLeft: responsiveWidth(-4)
+    },
+    panjang:{
+        marginTop: responsiveHeight(31),
+        fontSize: responsiveFontSize(2),
+        position:'absolute',
+        marginLeft: responsiveWidth(50)
+    },
+    coverimg: {
+        width: responsiveWidth(55),
+        height: responsiveHeight(18),
+        flexDirection:'row',
+        alignSelf:'center'
+    },
+    kotakHasil: {
+        flexDirection:'row',
+        backgroundColor: 'white',
+        width: responsiveWidth(81),
+        marginBottom: responsiveHeight(1),
+        borderRadius: 4,
+        shadowColor: 'black',
+        alignSelf:'center',
+        height:responsiveHeight(15),
+        marginTop: responsiveHeight(8),
+        shadowColor: "#000",
+        shadowOffset: {width: 0,height: 4,},
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9
+    },
+    textHasil: {
+        fontSize: responsiveFontSize(2.6),
+        marginLeft: responsiveWidth(5),
+        marginTop: responsiveHeight(2)
+    },
+    coverTextInput: {
+        alignSelf:'center',
+        marginTop: responsiveHeight(1)
+    },
+    textInput: {
+        borderWidth:responsiveWidth(0.5),
+        borderColor:'#E8E8E8',
+        borderRadius: responsiveWidth(6),
+        height: responsiveHeight(7.7),
+        fontSize: responsiveFontSize(1.8),
+        paddingLeft: responsiveWidth(5),
+        paddingRight: responsiveWidth(2),
+        backgroundColor: 'white',
+        width: responsiveWidth(80),
+        marginRight: responsiveWidth(0),
+        shadowColor: "#000",
+        shadowOffset: {width: 0,height: 4,},
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+        marginTop: responsiveHeight(1.5)
+    },
+    coverButton:{
+        width: responsiveWidth(80),
+        backgroundColor: '#00acc1',
+        height: responsiveHeight(6),
+        borderRadius: responsiveWidth(3),
+        alignSelf:'center',
+        marginTop: responsiveHeight(3)
+    },
+    textButton: {
+        fontSize:responsiveFontSize(1.6),
+        fontWeight:'bold',
+        color:'white',
+        textAlign:'center',
+        marginTop: responsiveHeight(2)
+    }
+})
+export default styles;
--- src/containers/pages/matematika/PersegiPanjang/index.js
+++ src/containers/pages/matematika/PersegiPanjang/index.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {View,Text,Image} from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
-
+import styles from './css';
 class PersegiPanjang extends Component {
     constructor(props){
         super(props);
@@ -44,24 +44,23 @@ class PersegiPanjang extends Component {
     render(){
         return(
             <ScrollView>
-                <Text style={{textAlign: 'center',fontSize: 20}}>Rumus Persegi Panjang</Text>
-                <View style={{flexDirection:'row',alignSelf:'center',marginLeft: 20,marginTop: 30}}>
-                <View style={{width: 210,height: 127,flexDirection:'row',alignSelf:'center'}}>
-                    <Image style={{width: '100%',height: '100%'}} source={require('../../../../assets/img/imgRumus/persegiPanjang.png')} />
+                <Text style={styles.header}>Rumus Persegi Panjang</Text>
+                <View style={styles.container}>
+                <View style={styles.coverimg}>
+                    <Image source={require('../../../../assets/img/imgRumus/persegiPanjang.png')} />
                 </View>
-                <Text style={{marginLeft: 10, margin: 0,marginTop: 50,fontSize: 20}}>{this.state.lebar}</Text>
+                <Text style={styles.lebar}>{this.state.lebar}</Text>
                 </View>
-                <Text style={{textAlign: 'center',fontSize: 20,marginLeft: 0,marginTop: 5}}>{this.state.panjang}</Text>
-                    <View style={{flexDirection:'row',backgroundColor: 'white',width: '80%',marginBottom: 5,borderRadius: 4,shadowColor: 'black',alignSelf:'center',height:90,marginTop: 50,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}>
-                        <Text style={{flex: 1,width: '100%', fontSize: 20,marginLeft: 20,marginTop: 10}}>{`Luas : ${this.state.luas}`}</Text>
-                        <Text style={{flex: 1,width: '100%', fontSize: 20,marginLeft: 20,marginTop: 10}}>{`Keliling : ${this.state.keliling}`}</Text>
+                <Text style={styles.panjang}>{this.state.panjang}</Text>
+                    <View style={styles.kotakHasil}>
+                        <Text style={styles.textHasil}>{`Luas : ${this.state.luas}`}</Text>
                     </View>
-                    <View style={{alignSelf:'center',marginTop: 20}}>
-                    <TextInput onChangeText={this.menanganiInput}  placeholder="masukan panjang" style={{borderWidth:1,borderColor:'#E8E8E8',borderRadius: 25,height:50,fontSize: 14,paddingLeft: 45,paddingRight: 25,backgroundColor: 'white',width: 330,marginRight: 10,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}/>
-                    <TextInput onChangeText={this.menanganiInput2}  placeholder="Masukan lebar" style={{marginTop:10, borderWidth:1,borderColor:'#E8E8E8',borderRadius: 25,height:50,fontSize: 14,paddingLeft: 45,paddingRight: 25,backgroundColor: 'white',width: 330,marginRight: 10,shadowColor: "#000",shadowOffset: {width: 0,height: 4,},shadowOpacity: 0.32,shadowRadius: 5.46,elevation: 9}}/>
+                    <View style={styles.coverTextInput}>
+                    <TextInput onChangeText={this.menanganiInput}  placeholder="masukan panjang" style={styles.textInput}/>
+                    <TextInput onChangeText={this.menanganiInput2}  placeholder="Masukan lebar" style={styles.textInput}/>
                     </View>
-                    <View style={{width: '78%',backgroundColor: '#00acc1',height: 'auto',borderRadius: 10,alignSelf:'center',marginTop: 15,height: 40}}>
-                    <Text onPress={() => this.hitungLuas()} style={{fontSize:13,fontWeight:'bold',color:'white',textAlign:'center',marginTop: 6,marginTop: 10}}>HITUNG LUAS</Text>    
+                    <View style={styles.coverButton}>
+                    <Text onPress={() => this.hitungLuas()} style={styles.textButton}>HITUNG LUAS</Text>    
                     </View>
             </ScrollView>
         )
